f8515834d9ec59743f4672676d955f5a939569b5
Fedchishina Victoria <fedchishina.victoria@gmail.com>
2017-09-19 22:27:30 +0300

    global setting of site

--- config/config.php
+++ config/config.php
@@ -3,3 +3,14 @@
 Config::set('site_title', 'tasks');
 
 Config::set('languages', array('en', 'ru'));
+
+// Routes. Route name => method prefix
+Config::set('routes', array(
+    'default' => '',
+    'admin' => 'admin_'
+));
+
+Config::set('default_route', 'default');
+Config::set('default_language', 'en');
+Config::set('default_controller', 'tasks');
+Config::set('default_action', 'index');
--- public/index.php
+++ public/index.php
@@ -5,3 +5,10 @@ define('ROOT', dirname(dirname(__FILE__)));
 
 require_once(ROOT.DS.'lib'.DS.'init.php');
 $router = new Router($_SERVER['REQUEST_URI']);
+/*echo "<pre>";
+print_r('route: '. $router->getRoute().PHP_EOL);
+print_r('language: '. $router->getLanguage().PHP_EOL);
+print_r('controller: '. $router->getController().PHP_EOL);
+print_r('action: '. $router->getMethodPrefix().$router->getAction().PHP_EOL);
+echo "Params:";
+print_r($router->getParams());*/
